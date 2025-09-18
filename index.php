c70ed602d2a74c92d3979feefbfd1a5e81a30950
Jean Lima <jeanmouralima2015@gmail.com>
2025-09-18 11:05:14 -0300

    update

--- index.php
+++ index.php
@@ -27,31 +27,7 @@
 
 <body class="index-page">
 
-  <header id="header" class="header d-flex align-items-center light-background sticky-top">
-    <div class="container-fluid position-relative d-flex align-items-center justify-content-between">
-
-      <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
-        <h1 class="sitename">Jean Moura Lima</h1>
-      </a>
-
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="index.html" class="active">Início</a></li>
-          <li><a href="about.html">Sobre</a></li>
-          <li><a href="resume.html">Currículo</a></li>
-          <li><a href="portfolio.html">Portfólio</a></li>
-          <li><a href="contact.html">Contato</a></li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
-
-      <div class="header-social-links">
-        <a href="https://github.com/jeancastrolima" target="_blank" class="github"><i class="bi bi-github"></i></a>
-        <a href="https://www.linkedin.com/in/jean-lima-59947a31b/" target="_blank" class="linkedin"><i class="bi bi-linkedin"></i></a>
-      </div>
-
-    </div>
-  </header>
+  <?php include 'navbar.php'?>
 
   <main class="main">
 
